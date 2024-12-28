8c3b30c8ebff5f2fdfb3aa558ef80c18fec437fe
Rancidgift57 <nnair855@gmail.com>
2024-12-28 17:28:35 +0530

    OUTPUT IN ARRAY

--- dice.java
+++ dice.java
@@ -1,17 +1,21 @@
+import java.util.ArrayList;
 public class dice{
     public static void main(String[] args){
         dice obj = new dice();
-        obj.Dice("",5);
+        System.out.println(obj.Dice("",5));
 
     }
 
-    public void Dice(String p ,int target){
+    public ArrayList<String> Dice(String p ,int target){ 
         if(target == 0){
-            System.out.println(p);
-            return;
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
         }
+        ArrayList<String> list = new ArrayList<>();
         for(int i = 1;i <= 6 && i <= target; i++){
-            Dice(p+i,target-i);
+            list.addAll(Dice(p+i,target-i));
         }
+        return list;
     }
 }
